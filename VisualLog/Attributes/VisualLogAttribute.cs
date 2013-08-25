9db1ed43a6691444925702e4234392f218a87734
unknown <retyk@RETYK1.emea.hpqcorp.net>
2013-08-25 16:34:42 +0300

    x

---
+++ VisualLog/Attributes/VisualLogAttribute.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace VisualLog.Attributes
+{
+    [AttributeUsage(AttributeTargets.Field | AttributeTargets.Property | AttributeTargets.Class, AllowMultiple = true, Inherited = true)]
+    public class VisualLogAttribute : Attribute
+    {
+        public string Description { set; get; }
+
+    }
+}
